b75f98399795bcd8c0aa59be972658496a0810e5
chenyi003 <dog30511@gmail.com>
2025-10-03 16:21:26 +0800

    1003php

---
+++ ch2.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>IF</h1>
+    <?php
+        $score= 99;
+        if($score>=60){
+            echo "及格";
+        }else{
+            echo "不及格";
+        }
+
+    ?>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=12, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h1>php Basic</h1>
+<p>
+    <a href="ch1.php">ch1 變數</a>
+    
+</p>
+<P>
+    <a href="ch2.php">ch2 if</a>
+
+</P>
+
+
+<?php
+$a=10;
+$b=20;
+$b=20;
+$c = $a+$b;
+$c = $a+$b;
+
+// echo $c;
+
+for ($i=1; $i < 10; $i++) { 
+   ECHO $i;
+   echo PHP_EOL;
+   echo "<br>";
+   
+}
+
+
+
+?>
+</body>
+</html>
